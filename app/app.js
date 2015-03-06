8d9ccec8cfc273a6924fb5b3e846a87cf2b6be42
Evan Turner <evturn@gmail.com>
2015-03-06 16:37:39 -0500

    register script required in index

--- app/app.js
+++ app/app.js
@@ -2,7 +2,8 @@
 
 angular.module('myApp', [
   'ngRoute',
-  'myApp.home'
+  'myApp.home',
+  'myApp.register'
 ]).
 config(['$routeProvider', function($routeProvider) {
      $routeProvider.otherwise({
