c18d3e6088d8c3e61061a476adb57ea7bb76c788
Evan Turner <evturn@gmail.com>
2015-03-06 16:04:13 -0500

    SignIn function written to authenticate user through email and password

--- app/home/home.js
+++ app/home/home.js
@@ -10,10 +10,22 @@ angular.module('myApp.home', ['ngRoute', 'firebase'])
 }])
 
 .controller('HomeCtrl', ['$scope', '$firebaseSimpleLogin', function() {
-	$scope.SignIn = function($scope) {
-		var username = $scope.user.email;
-		var password = $scope.user.password;
-	}
 	var firebaseObj = new Firebase("https://blogz-on-fire.firebaseio.com/");
 	var loginObj = $firebaseSimpleLogin(firebaseObj);
+
+	$scope.SignIn = function(event) {
+		event.preventDefault();
+		var username = $scope.user.email;
+		var password = $scope.user.password;
+
+		loginObj.$login('password', {
+			email: username,
+			password: password
+		})
+		.then(function(user) {
+			console.log('Authentication successful');
+		}, function(error) {
+			console.log('Authentication failure');
+		});
+	}
 }]);
