b4e0986853a3c184629c888a7b9bb7c7aad68032
Evan Turner <evturn@gmail.com>
2015-03-06 17:28:25 -0500

    signup function created

--- app/register/register.js
+++ app/register/register.js
@@ -12,10 +12,19 @@ angular.module('myApp.register', ['ngRoute', 'firebase'])
 .controller('RegisterCtrl', ['$scope', '$firebaseAuth', function($scope, $firebaseAuth) {
 	var firebaseObj = new Firebase("https://blogz-on-fire.firebaseio.com/");
 	var auth = $firebaseAuth(firebaseObj);
-	
+
  	$scope.signUp = function() {
  		if (!$scope.regForm.$invalid) {
- 			console.log('Valid form submission');
+ 			var email = $scope.user.email;
+ 			var password = $scope.user.password;
+ 			if (email && password) {
+ 				auth.$createUser(email, password)
+ 					.then(function() {
+ 						console.log('User creation success');
+ 					}, function(error) {
+ 						console.log(error);
+ 					});
+ 			}
  		}
  	};
 }]);
