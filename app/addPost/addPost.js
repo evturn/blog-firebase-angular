bc2a6ae8e63331ab0c97ed2d5edbeb5c98c4153b
Evan Turner <evturn@gmail.com>
2015-03-07 17:46:39 -0500

    posts being created on firebase cloud

--- app/addPost/addPost.js
+++ app/addPost/addPost.js
@@ -1,29 +1,30 @@
 'use strict';
- 
+
 angular.module('myApp.addPost', ['ngRoute'])
- 
+
 .config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/addPost', {
     templateUrl: 'addPost/addPost.html',
     controller: 'AddPostCtrl'
   });
 }])
- 
-.controller('AddPostCtrl', ['$scope', '$firebase', function($scope, $firebase) {
- 	$scope.AddPost = function() {
- 		var firebaseObj = new Firebase("https://blogz-on-fire.firebaseio.com/");
- 		var fb = $firebase(firebaseObj);
 
- 		var title = $scope.article.title;
- 		var post = $scope.article.post;
+.controller('AddPostCtrl', ['$scope','$firebase',function($scope, $firebase) {
+    $scope.AddPost = function(){
+			var title = $scope.article.title;
+  		var post = $scope.article.post;
+	
+			var firebaseObj = new Firebase("https://blogz-on-fire.firebaseio.com");
+    	var fb = $firebase(firebaseObj);
 
- 		fb.$push({
-    	title: title,
-	    post: post
-		}).then(function(ref) {
-		    console.log(ref);
-		}, function(error) {
-		    console.log("Error:", error);
-		});
- 	}
+			fb.$push({
+				title: title,
+				post: post
+			}).then(function(ref) {
+		  		console.log(ref); 
+			}, function(error) {
+		  		console.log("Error:", error);
+			});
+
+		}
 }]);
